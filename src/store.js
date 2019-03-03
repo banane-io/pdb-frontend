8ded6838a65be8949b9b0e44999729034c2666b2
Marc-Andre Girard <marcandr.girard@gmail.com>
2019-03-03 15:00:01 -0500

    Added neighbors in the grid view

--- src/store.js
+++ src/store.js
@@ -7,16 +7,25 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     grid: [],
+    neighbors: {},
   },
   mutations: {
     updateGrid(state, grid) {
       state.grid = grid;
+    },
+    updateNeighbors(state, neighbors) {
+      state.neighbors = neighbors;
     },
   },
   actions: {
     getGrid({ commit }) {
       axios.get('/api/grid')
         .then(result => commit('updateGrid', result.data))
+        .catch(console.error);
+    },
+    getNeighbors({ commit }) {
+      axios.get('/api/grid/neighbors')
+        .then(result => commit('updateNeighbors', result.data))
         .catch(console.error);
     },
   },
--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -1,18 +1,31 @@
 <template>
-    <div>
-        <h1>Grid system</h1>
-        <div class='row'>
-            <div class='centered'>
-                <table class='table-bordered float inline'>
-                    <tr v-for="(lines, index) in grid" :key="index">
-                        <td v-for="(zone) in lines" :key="zone.id" :class="zone.terrain">
-                            <input type="radio" :id="zone.id" :value="zone.id" name="grid"  class="grid_point"/>
-                        </td>
-                    </tr>
-                </table>
+  <div>
+    <h1>Grid system</h1>
+    <div class="row">
+      <div class="float">
+        <table class="table-bordered inline centered">
+          <tr v-for="(lines, index) in grid" :key="index">
+            <td v-for="(zone) in lines" :key="zone.id" :class="zone.terrain">
+              <input type="radio" :id="zone.id" :value="zone.id" name="grid" class="grid_point">
+            </td>
+          </tr>
+        </table>
+        <div class='float inline square border'>
+            <div class='row centered'>
+                <a v-if="neighbors.NORTH" href='#' class='btn glyphicon glyphicon-arrow-up'>UP</a>
+            </div>
+            <div class='row'>
+                <a v-if="neighbors.WEST" href='#' class='btn glyphicon glyphicon-arrow-left'>LEFT</a>
+                <a v-if="neighbors.EAST" href='#' class='btn glyphicon glyphicon-arrow-right'>RIGHT</a>
+            </div>
+            <div class='row centered'>
+                <a v-if="neighbors.SOUTH" href='#'
+                    class='btn glyphicon glyphicon-arrow-down centered'>DOWN</a>
             </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -20,10 +33,14 @@ export default {
   name: 'Grid',
   created() {
     this.$store.dispatch('getGrid');
+    this.$store.dispatch('getNeighbors');
   },
   computed: {
     grid() {
       return this.$store.state.grid || [];
+    },
+    neighbors() {
+      return this.$store.state.neighbors || {};
     },
   },
 };
@@ -31,21 +48,21 @@ export default {
 
 <style scoped>
 .PLAIN {
-    background-color: green;
+  background-color: green;
 }
 .SWAMP {
-    background-color: DarkGreen;
+  background-color: DarkGreen;
 }
 .BEACH {
-    background-color: yellow;
+  background-color: yellow;
 }
 .MOUNTAIN {
-    background-color: gray;
+  background-color: gray;
 }
 .WATER {
-    background-color: blue;
+  background-color: blue;
 }
 .BRIDGE {
-    background-color: SaddleBrown;
+  background-color: SaddleBrown;
 }
 </style>
