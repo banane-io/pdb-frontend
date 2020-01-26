6e888a7fc68f4ce3bbb6ad5b57cd6038eb93801d
Marc-Andre Girard <marcandr.girard@gmail.com>
2020-01-26 09:33:58 -0500

    Fix problems from updating dependencies
    It seems that now we need to use () for arrow funcion.

--- src/store.js
+++ src/store.js
@@ -42,42 +42,42 @@ export default new Vuex.Store({
       console.log(`Calling /api/grid/${id}`);
       axios
         .get(`/api/grid/${id}`)
-        .then(result => commit('updateGrid', result.data))
+        .then((result) => commit('updateGrid', result.data))
         .catch(console.error);
     },
     getNeighbors({ commit }, id) {
       console.log(`Calling /api/grid/neighbors/${id}`);
       axios
         .get(`/api/grid/neighbors/${id}`)
-        .then(result => commit('updateNeighbors', result.data))
+        .then((result) => commit('updateNeighbors', result.data))
         .catch(console.error);
     },
     getZone({ commit }, id) {
       console.log(`Calling /api/mapPoint/${id}`);
       axios
         .get(`/api/mapPoint/${id}`)
-        .then(result => commit('updateCurrentZone', result.data))
+        .then((result) => commit('updateCurrentZone', result.data))
         .catch(console.error);
     },
     getHero({ commit }) {
       console.log('Calling /api/hero');
       axios
         .get('/api/hero')
-        .then(result => commit('updateHero', result.data))
+        .then((result) => commit('updateHero', result.data))
         .catch(console.error);
     },
     getZoneActions({ commit }, id) {
       console.log(`Calling /api/mapPoint/${id}/actions`);
       axios
         .get(`/api/mapPoint/${id}/actions`)
-        .then(result => commit('updateCurrentZoneActions', result.data))
+        .then((result) => commit('updateCurrentZoneActions', result.data))
         .catch(console.error);
     },
     getBaseResources({ commit }) {
       console.log('Calling /api/base/resources');
       axios
         .get('/api/base/resources')
-        .then(result => commit('updateBaseResources', result.data))
+        .then((result) => commit('updateBaseResources', result.data))
         .catch(console.error);
     },
   },
