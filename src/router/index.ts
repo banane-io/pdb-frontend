03b89c929c0d5f389991426bd6ec394cd58e7903
Marc-André Girard <marcandr.girard@gmail.com>
2025-06-05 16:34:38 -0400

    Adding Registration view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import OauthCallback from '../views/OauthCallback.vue';
-
+import OauthCallback from '../views/OauthCallback.vue'
+import Registration from '../views/RegistrationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Registration,
     },
     {
       path: '/oauth-callback',
